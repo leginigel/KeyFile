135634da6bc2898f52c1ce2e49440c763dae33ef
leginigel <38337846+leginigel@users.noreply.github.com>
2019-08-07 08:09:23 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
